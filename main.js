a071cf0bd944231f1be83beb752775dbbdac58a2
blueprincess <nadiamazouz7@gmail.com>
2017-05-01 13:19:25 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(fucntion(){
+
+//ici, $(this) est le document
+var doc= $(this);
+
+$(".content").mouseout(function(event){
+
+//ici, $(this) est la div de classe content
+var p = $(this).children("p");
+
+p.each(function (index) {
+
+//ici, $(this) sera à chaque fois un des paragraphes p.
+$(this).prepend("Partie "+index);
+
+  });
+
+ });
+
+});
